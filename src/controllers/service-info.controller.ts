03414e19e9b7152de0dcd272259ed72ae292a09c
Will O'Brien <1069495+willscripted@users.noreply.github.com>
2025-09-02 14:30:28 -0400

    feat: add version information endpoint and include in sentry info

--- src/controllers/service-info.controller.ts
+++ src/controllers/service-info.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
 
+const {
+  ORG_OPENCONTAINERS_IMAGE_VERSION,
+  ORG_OPENCONTAINERS_IMAGE_REVISION,
+  ORG_OPENCONTAINERS_IMAGE_CREATED
+} = process.env
+
+
 export default class ServiceInfoController {
   public static healthz(req: Request, res: Response) {
     return res.status(200).json({ ok: true }).end()
@@ -11,5 +18,15 @@ export default class ServiceInfoController {
 
   public static metrics(req: Request, res: Response) {
     return res.status(200).json({ ok: true }).end()
+  }
+
+  public static version(req: Request, res: Response) {
+    return res.status(200).json({
+      version: ORG_OPENCONTAINERS_IMAGE_VERSION,
+      revision: ORG_OPENCONTAINERS_IMAGE_REVISION,
+      created: {
+        utc: ORG_OPENCONTAINERS_IMAGE_CREATED,
+      }
+    }).end()
   }
 }
