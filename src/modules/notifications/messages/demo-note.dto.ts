4047861ae0580743b8cad3a2fe7baf6d06726fd6
Will O'Brien <1069495+willscripted@users.noreply.github.com>
2025-09-17 16:05:02 -0400

    copyedit(demo): change demo notification text for matt

--- src/modules/notifications/messages/demo-note.dto.ts
+++ src/modules/notifications/messages/demo-note.dto.ts
@@ -6,7 +6,7 @@ import { ApnsPriority } from "../providers/apple-push-notification.service";
 export class DemoNote implements Message {
 
   constructor() {
-    this.title = "Demo message Title!"
+    this.title = "Hi Matt it's Will"
     this.body = "Demo message body content"
     this.apnsPriority = ApnsPriority.SEND_IMMEDIATELY
     this.apnsExpiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
